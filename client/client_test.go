e2103805a0c88e885f6680da8927fef3bf14fa6d
Sergio Rubio <rubiojr@frameos.org>
2021-12-23 11:43:31 +0100

    Add a testutil package

--- client/client_test.go
+++ client/client_test.go
@@ -2,16 +2,11 @@ package client
 
 import (
 	"context"
-	"fmt"
-	"net"
 	"os"
 	"testing"
-	"time"
 
-	"github.com/charmbracelet/charm/client"
 	cfs "github.com/charmbracelet/charm/fs"
-	"github.com/charmbracelet/charm/server"
-	"github.com/charmbracelet/keygen"
+	"github.com/rubiojr/tavern/internal/testutil"
 	ts "github.com/rubiojr/tavern/server"
 	"github.com/stretchr/testify/assert"
 )
@@ -21,25 +16,7 @@ func TestPublish(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	cfg := server.DefaultConfig()
-	cfg.DataDir = tdir
-
-	sp := fmt.Sprintf("%s/.ssh", cfg.DataDir)
-	kp, err := keygen.NewWithWrite(sp, "charm_server", []byte(""), keygen.RSA)
-	if err != nil {
-		assert.FailNow(t, "error generating server keys")
-	}
-	cfg.WithKeys(kp.PublicKey, kp.PrivateKeyPEM)
-
-	charm, err := server.NewServer(cfg)
-	if err != nil {
-		panic(err)
-	}
-	go charm.Start(ctx)
-
-	if !waitForServer(":35354") {
-		t.Fatal("charm server did not start")
-	}
+	err := testutil.StartCharmServer(ctx, tdir)
 
 	tav := ts.NewServerWithConfig(&ts.Config{
 		Addr:           "127.0.0.2:8000",
@@ -50,27 +27,17 @@ func TestPublish(t *testing.T) {
 
 	// Create a new client.
 	tcc := DefaultConfig()
-	tcc.CharmServerURL = "http://127.0.0.2"
-	tcc.ServerURL = "http://127.0.0.2:8000"
+	tcc.CharmServerURL = testutil.CharmServerURL
+	tcc.ServerURL = testutil.ServerURL
 	c, err := NewClientWithConfig(tcc)
 	if err != nil {
-		t.Fatal(err)
+		assert.FailNow(t, "error creating tavern client")
 	}
 
-	// Charm inits
-	err = genClientKeys("127.0.0.2")
-	assert.NoError(t, err)
-
-	cconfig, err := client.ConfigFromEnv()
+	cc, err := testutil.CharmClient()
 	if err != nil {
-		assert.FailNow(t, "error loading charm client config")
+		assert.FailNow(t, "error starting charm client")
 	}
-	cconfig.Host = "127.0.0.2"
-
-	cc, err := client.NewClient(cconfig)
-	assert.NoError(t, err)
-	cc.Config.SSHPort = 35353
-	cc.Config.HTTPPort = 35354
 
 	charmfs, err := cfs.NewFSWithClient(cc)
 	if err != nil {
@@ -85,31 +52,4 @@ func TestPublish(t *testing.T) {
 
 	err = c.Publish("testdata/test.txt")
 	assert.NoError(t, err)
-}
-
-func waitForServer(addr string) bool {
-	for i := 0; i < 40; i++ {
-		conn, err := net.Dial("tcp", addr)
-		if err == nil {
-			conn.Close()
-			return true
-		}
-		time.Sleep(500 * time.Millisecond)
-	}
-
-	return false
-}
-
-func genClientKeys(dir string) error {
-	// Generate keys
-	dp, err := client.DataPath(dir)
-	if err != nil {
-		return err
-	}
-	_, err = keygen.NewWithWrite(dp, "charm", []byte(""), keygen.RSA)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
---
+++ internal/testutil/testutil.go
@@ -0,0 +1,90 @@
+package testutil
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/charmbracelet/charm/client"
+	"github.com/charmbracelet/charm/server"
+	"github.com/charmbracelet/keygen"
+)
+
+const TestHost = "127.0.0.2"
+const CharmServerURL = "http://" + TestHost
+const ServerURL = "http://" + TestHost + ":8000"
+
+func StartCharmServer(ctx context.Context, dataDir string) error {
+	cfg := server.DefaultConfig()
+	cfg.DataDir = dataDir
+
+	sp := fmt.Sprintf("%s/.ssh", cfg.DataDir)
+	kp, err := keygen.NewWithWrite(sp, "charm_server", []byte(""), keygen.RSA)
+	if err != nil {
+		return err
+	}
+	cfg.WithKeys(kp.PublicKey, kp.PrivateKeyPEM)
+
+	charm, err := server.NewServer(cfg)
+	if err != nil {
+		panic(err)
+	}
+	go charm.Start(ctx)
+
+	if !waitForServer(":35354") {
+		return fmt.Errorf("charm server did not start")
+	}
+
+	return nil
+}
+
+func CharmClient() (*client.Client, error) {
+	err := genClientKeys("127.0.0.2")
+	if err != nil {
+		return nil, err
+	}
+
+	cconfig, err := client.ConfigFromEnv()
+	if err != nil {
+		return nil, err
+	}
+	cconfig.Host = "127.0.0.2"
+
+	cc, err := client.NewClient(cconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	cc.Config.SSHPort = 35353
+	cc.Config.HTTPPort = 35354
+
+	return cc, nil
+}
+
+func waitForServer(addr string) bool {
+	for i := 0; i < 40; i++ {
+		conn, err := net.Dial("tcp", addr)
+		if err == nil {
+			conn.Close()
+			return true
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	return false
+}
+
+func genClientKeys(dir string) error {
+	// Generate keys
+	dp, err := client.DataPath(dir)
+	if err != nil {
+		return err
+	}
+	_, err = keygen.NewWithWrite(dp, "charm", []byte(""), keygen.RSA)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
