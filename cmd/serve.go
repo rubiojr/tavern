3085a31a16f88bfb9a10627e936c338c717f2b24
Sergio Rubio <rubiojr@frameos.org>
2022-02-04 12:16:44 +0100

    No longer required

--- cmd/serve.go
+++ cmd/serve.go
@@ -25,13 +25,11 @@ var serveCmd = &cobra.Command{
 
 var path *string
 var addr *string
-var charmServerURL *string
 var issuers *[]string
 
 func init() {
 	rootCmd.AddCommand(serveCmd)
 	path = serveCmd.Flags().StringP("path", "p", server.ServerDefaultUploadsPath, "Path where the files will be uploaded/served")
 	addr = serveCmd.Flags().StringP("address", "a", server.ServerDefaultAddr, "Listening address")
-	charmServerURL = serveCmd.Flags().StringP("charm-server-url", "", server.ServerDefaultCharmServerURL, "Charm server URL address")
 	issuers = serveCmd.Flags().StringSliceP("allowed-charm-servers", "w", []string{}, "Allowed Charm servers")
 }
