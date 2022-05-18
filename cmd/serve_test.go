0657444ec7516c8b3e556360cc7929f669628246
Sergio Rubio <rubiojr@rbel.co>
2022-05-18 18:36:54 +0200

    Fix test flakiness
    Use subtests to control concurrency

--- cmd/serve_test.go
+++ cmd/serve_test.go
@@ -27,143 +27,146 @@ import (
 const charmID = "b4ede63d-c736-4561-80e9-0f912337b251"
 
 func TestServe(t *testing.T) {
-	buf := &testutil.Buffer{}
-	log.SetOutput(buf)
-	tdir := t.TempDir()
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	serverAddr := "127.0.0.1:8001"
 
-	rootCmd.SetArgs([]string{
-		"serve",
-		"--path", tdir,
-		"--address", testutil.TestServerAddr,
+	t.Run("test serve", func(t *testing.T) {
+		buf := &testutil.Buffer{}
+		log.SetOutput(buf)
+		tdir := t.TempDir()
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		rootCmd.SetArgs([]string{
+			"serve",
+			"--path", tdir,
+			"--address", serverAddr,
+		})
+		go rootCmd.ExecuteContextC(ctx)
+
+		if !testutil.WaitForServer(serverAddr) {
+			assert.FailNow(t, "error starting tavern server")
+		}
+
+		assert.True(t, strings.Contains(buf.String(), fmt.Sprintf(`uploads directory: %s`, tdir)))
+		assert.Regexp(t, regexp.MustCompile(`serving on: `+serverAddr), buf.String())
 	})
-	go rootCmd.ExecuteContextC(ctx)
 
-	if !testutil.WaitForServer(testutil.TestServerAddr) {
-		assert.FailNow(t, "error starting tavern server")
-	}
+	t.Run("invalid JWT", func(t *testing.T) {
+		buf := &testutil.Buffer{}
+		log.SetOutput(buf)
+		tdir := t.TempDir()
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-	assert.True(t, strings.Contains(buf.String(), fmt.Sprintf(`uploads directory: %s`, tdir)))
-	assert.Regexp(t, regexp.MustCompile(`serving on: `+testutil.TestServerAddr), buf.String())
-}
+		rootCmd.SetArgs([]string{
+			"serve",
+			"--path", tdir,
+			"--address", serverAddr,
+		})
+		go rootCmd.ExecuteContextC(ctx)
 
-func TestInvalidJWT(t *testing.T) {
-	buf := &testutil.Buffer{}
-	log.SetOutput(buf)
-	tdir := t.TempDir()
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+		if !testutil.WaitForServer(serverAddr) {
+			assert.FailNow(t, "error starting tavern server")
+		}
 
-	rootCmd.SetArgs([]string{
-		"serve",
-		"--path", tdir,
-		"--address", testutil.TestServerAddr,
+		pk, err := generateEd25519Keys()
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+		token := jwt.NewWithClaims(&jwt.SigningMethodEd25519{}, &jwt.RegisteredClaims{
+			Issuer:    "http://localhost:35354",
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
+			Audience:  []string{"tavern"},
+			Subject:   charmID,
+		})
+		sum := sha256.Sum256(*pk)
+		kid := fmt.Sprintf("%x", sum)
+		token.Header["kid"] = kid
+		tokenString, err := token.SignedString(pk)
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+		c, err := tClient()
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+		bbuf := bytes.NewBuffer([]byte{})
+		req, err := c.UploadRequest(tokenString, bbuf)
+		assert.NoError(t, err)
+		httpc := &http.Client{}
+		resp, err := httpc.Do(req)
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+
+		assert.Equal(t, 401, resp.StatusCode)
+		assert.Regexp(t, regexp.MustCompile(`JWT validation failed`), buf.String())
 	})
-	go rootCmd.ExecuteContextC(ctx)
+	t.Run("valid JWT", func(t *testing.T) {
+		buf := &testutil.Buffer{}
+		log.SetOutput(buf)
+		tdir := t.TempDir()
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-	if !testutil.WaitForServer(testutil.TestServerAddr) {
-		assert.FailNow(t, "error starting tavern server")
-	}
+		rootCmd.SetArgs([]string{
+			"serve",
+			"--path", tdir,
+			"--address", serverAddr,
+		})
+		go rootCmd.ExecuteContextC(ctx)
 
-	pk, err := generateEd25519Keys()
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-	token := jwt.NewWithClaims(&jwt.SigningMethodEd25519{}, &jwt.RegisteredClaims{
-		Issuer:    "http://localhost:35354",
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
-		Audience:  []string{"tavern"},
-		Subject:   charmID,
+		if !testutil.WaitForServer(serverAddr) {
+			assert.FailNow(t, "error starting tavern server")
+		}
+
+		rpk, err := ioutil.ReadFile("../_fixtures/server/.ssh/charm_server_ed25519")
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+		pk, err := gossh.ParseRawPrivateKey(rpk)
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+		jwkp := server.NewJSONWebKeyPair(pk.(*ed25519.PrivateKey))
+
+		token := jwt.NewWithClaims(&jwt.SigningMethodEd25519{}, &jwt.RegisteredClaims{
+			Issuer:    "http://localhost:35354",
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
+			Audience:  []string{"tavern"},
+			Subject:   charmID,
+		})
+		sum := sha256.Sum256([]byte(*jwkp.PrivateKey))
+		kid := fmt.Sprintf("%x", sum)
+		token.Header["kid"] = kid
+		tokenString, err := token.SignedString(pk)
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+
+		c, err := tClient()
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+
+		req, err := c.UploadRequest(tokenString, bytes.NewBuffer([]byte{}))
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+
+		httpc := &http.Client{}
+		resp, err := httpc.Do(req)
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+
+		rout, err := io.ReadAll(resp.Body)
+		if err != nil {
+			assert.FailNow(t, err.Error())
+		}
+		assert.Equal(t, 400, resp.StatusCode)
+		assert.Equal(t, "no files found in request\n", string(rout))
 	})
-	sum := sha256.Sum256(*pk)
-	kid := fmt.Sprintf("%x", sum)
-	token.Header["kid"] = kid
-	tokenString, err := token.SignedString(pk)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-	c, err := tClient()
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-	bbuf := bytes.NewBuffer([]byte{})
-	req, err := c.UploadRequest(tokenString, bbuf)
-	assert.NoError(t, err)
-	httpc := &http.Client{}
-	resp, err := httpc.Do(req)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-
-	assert.Equal(t, 401, resp.StatusCode)
-	assert.Regexp(t, regexp.MustCompile(`JWT validation failed`), buf.String())
-}
-
-func TestValidJWT(t *testing.T) {
-	buf := &testutil.Buffer{}
-	log.SetOutput(buf)
-	tdir := t.TempDir()
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	rootCmd.SetArgs([]string{
-		"serve",
-		"--path", tdir,
-		"--address", testutil.TestServerAddr,
-	})
-	go rootCmd.ExecuteContextC(ctx)
-
-	if !testutil.WaitForServer(testutil.TestServerAddr) {
-		assert.FailNow(t, "error starting tavern server")
-	}
-
-	rpk, err := ioutil.ReadFile("../_fixtures/server/.ssh/charm_server_ed25519")
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-	pk, err := gossh.ParseRawPrivateKey(rpk)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-	jwkp := server.NewJSONWebKeyPair(pk.(*ed25519.PrivateKey))
-
-	token := jwt.NewWithClaims(&jwt.SigningMethodEd25519{}, &jwt.RegisteredClaims{
-		Issuer:    "http://localhost:35354",
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
-		Audience:  []string{"tavern"},
-		Subject:   charmID,
-	})
-	sum := sha256.Sum256([]byte(*jwkp.PrivateKey))
-	kid := fmt.Sprintf("%x", sum)
-	token.Header["kid"] = kid
-	tokenString, err := token.SignedString(pk)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-
-	c, err := tClient()
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-
-	req, err := c.UploadRequest(tokenString, bytes.NewBuffer([]byte{}))
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-
-	httpc := &http.Client{}
-	resp, err := httpc.Do(req)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-
-	rout, err := io.ReadAll(resp.Body)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-	assert.Equal(t, 400, resp.StatusCode)
-	assert.Equal(t, "no files found in request\n", string(rout))
 }
 
 // generateEd25519Keys creates a pair of EdD25519 keys for SSH auth.
@@ -179,7 +182,7 @@ func generateEd25519Keys() (*ed25519.PrivateKey, error) {
 
 func tClient() (*client.Client, error) {
 	cfg := client.DefaultConfig()
-	cfg.ServerURL = testutil.TestServerURL
+	cfg.ServerURL = "http://127.0.0.1:8001"
 	cfg.CharmServerHost = testutil.CharmServerHost
 	return client.NewClientWithConfig(cfg)
 }
